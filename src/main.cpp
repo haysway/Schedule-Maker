8af5681e1658b8f9ba5b34481c25e136158d9044
Hayden Swain-Schmit <swainschmity@gmail.com>
2024-09-08 15:13:01 -0600

    correct file path

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ int main()
     }
     cout << "Total shifts this month: " << totalShifts << endl;
 
-    schedule.printScheduleToCVS("scheduleFile.csv");
+    schedule.printScheduleToCVS("C:/Users/swain/Schedule Maker/src/scheduleFile.csv");
 
     for (Worker* worker : workers) {
         delete worker;
